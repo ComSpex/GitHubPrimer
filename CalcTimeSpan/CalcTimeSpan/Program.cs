560a547ad1a934b4116e878bffbdce02792be1ed
Yosuke Maeda <yosuke_maeda@hotmail.com>
2016-08-02 20:27:27 +0900

    1.2

--- CalcTimeSpan/CalcTimeSpan/Program.cs
+++ CalcTimeSpan/CalcTimeSpan/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.IO;
 using System.Windows;
-using System.Windows.Media;
-using System.Windows.Controls;
 
 namespace CalcTimeSpan {
 	class Program {
@@ -13,6 +11,7 @@ namespace CalcTimeSpan {
 		static bool skip = false,quit=false;
 		[STAThread]
 		static void Main(string[] args) {
+			Console.Title="";
 			Console.WriteLine("You can (q)uit anytime.");
 			for(;;) {
 				Console.Write("Current TimeSpan : ");
@@ -35,7 +34,8 @@ namespace CalcTimeSpan {
 				}
 			}
 			if(!skip) {
-				Supplement();
+				//Supplement();
+				fillWithData();
 			}
 		}
 		private static bool isQuit(string duration) {
@@ -71,7 +71,6 @@ namespace CalcTimeSpan {
 					Console.ForegroundColor=ConsoleColor.Cyan;
 					Console.WriteLine("Successfully written to the Clipboard.");
 					Console.ResetColor();
-					fillWithData();
 				} catch(Exception ex) {
 					Console.ForegroundColor=ConsoleColor.Red;
 					Console.WriteLine(ex.Message);
@@ -82,82 +81,6 @@ namespace CalcTimeSpan {
 		private static void fillWithData() {
 			Application app = new Application();
 			app.Run(new YouTubeMusicHelper(dic));
-		}
-	}
-	public class YouTubeMusicHelper:Window {
-		protected Dictionary<TimeSpan,TimeSpan> dics;
-		public YouTubeMusicHelper(Dictionary<TimeSpan,TimeSpan> dict) {
-			dics=dict;
-			Width=1024/2;
-			Height=768/2;
-			Title="YouTube music title editor";
-			this.Content=Edit();
-		}
-		object Edit() {
-			ListBox lb = new ListBox();
-			lb.HorizontalContentAlignment=HorizontalAlignment.Stretch;
-			int i = 1;
-			foreach(KeyValuePair<TimeSpan,TimeSpan> dic in dics) {
-				lb.Items.Add(Edit(i,dic));
-				++i;
-			}
-			return lb;
-		}
-		private object Edit(int index,KeyValuePair<TimeSpan,TimeSpan> dic) {
-			ListBoxItem item = new ListBoxItem();
-			item.HorizontalContentAlignment=HorizontalAlignment.Stretch;
-			Grid grid = new Grid();
-			grid.HorizontalAlignment=HorizontalAlignment.Stretch;
-			grid.Margin=new Thickness(0,1,0,1);
-			grid.ColumnDefinitions.Add(new ColumnDefinition());
-			grid.ColumnDefinitions.Add(new ColumnDefinition());
-			grid.ColumnDefinitions.Add(new ColumnDefinition());
-			grid.ColumnDefinitions.Add(new ColumnDefinition());
-			for(int i = 0;i<grid.ColumnDefinitions.Count;++i) {
-				grid.ColumnDefinitions[i].Width=GridLength.Auto;
-			}
-			{
-				TextBlock tb = new TextBlock();
-				tb.Text=String.Format("{0,3}",index);
-				tb.VerticalAlignment=VerticalAlignment.Center;
-				tb.Margin=new Thickness(0,0,2,0);
-				grid.Children.Add(tb);
-				Grid.SetColumn(tb,0);
-			}
-			{
-				TextBox tb = new TextBox();
-				grid.Children.Add(tb);
-				Grid.SetColumn(tb,1);
-				grid.ColumnDefinitions[1].Width=new GridLength(250,GridUnitType.Star);
-			}
-			{
-				TextBox tb = new TextBox();
-				tb.Text=String.Format("{0:HH:mm:ss}",dic.Key);
-				tb.HorizontalContentAlignment=HorizontalAlignment.Right;
-				grid.Children.Add(tb);
-				Grid.SetColumn(tb,2);
-				grid.ColumnDefinitions[2].Width=new GridLength(80,GridUnitType.Star);
-			}
-			{
-				TextBlock tb = new TextBlock();
-				tb.Text=String.Format("{0:HH:mm:ss}",dic.Value);
-				tb.HorizontalAlignment=HorizontalAlignment.Right;
-				tb.VerticalAlignment=VerticalAlignment.Center;
-				grid.Children.Add(tb);
-				Grid.SetColumn(tb,3);
-				grid.ColumnDefinitions[3].Width=new GridLength(60,GridUnitType.Star);
-			}
-			item.Content=grid;
-			return item;
-		}
-		object HelloWPFagain() {
-			Grid gr = new Grid();
-			TextBlock tb = new TextBlock();
-			tb.Text="Hello, WPF again.";
-			gr.Children.Add(tb);
-			Viewbox box = new Viewbox();
-			box.Child=gr;
-			return box;
 		}
 	}
 }
---
+++ CalcTimeSpan/CalcTimeSpan/YouTubeMusicHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace CalcTimeSpan {
+	public class YouTubeMusicHelper:Window {
+		protected Dictionary<TimeSpan,TimeSpan> dics;
+		public YouTubeMusicHelper(Dictionary<TimeSpan,TimeSpan> dict) {
+			dics=dict;
+			Width=1024/2;
+			Height=768/2;
+			Title="YouTube music title editor";
+			this.Content=Edit();
+		}
+		object Edit() {
+			ListBox lb = new ListBox();
+			lb.SelectionChanged+=(sender,e) => {
+				ListBoxItem item = e.AddedItems[0] as ListBoxItem;
+				Grid grid = item.Content as Grid;
+				TextBox tb = grid.Children[1] as TextBox;
+				tb.SelectAll();
+				tb.Focus();
+			};
+			lb.HorizontalContentAlignment=HorizontalAlignment.Stretch;
+			int i = 1;
+			foreach(KeyValuePair<TimeSpan,TimeSpan> dic in dics) {
+				lb.Items.Add(Edit(i,dic));
+				++i;
+			}
+			return lb;
+		}
+		private object Edit(int index,KeyValuePair<TimeSpan,TimeSpan> dic) {
+			ListBoxItem item = new ListBoxItem();
+			item.HorizontalContentAlignment=HorizontalAlignment.Stretch;
+			Grid grid = new Grid();
+			grid.HorizontalAlignment=HorizontalAlignment.Stretch;
+			grid.Margin=new Thickness(0,1,0,1);
+			grid.ColumnDefinitions.Add(new ColumnDefinition());
+			grid.ColumnDefinitions.Add(new ColumnDefinition());
+			grid.ColumnDefinitions.Add(new ColumnDefinition());
+			grid.ColumnDefinitions.Add(new ColumnDefinition());
+			for(int i = 0;i<grid.ColumnDefinitions.Count;++i) {
+				grid.ColumnDefinitions[i].Width=GridLength.Auto;
+			}
+			{
+				TextBlock tb = new TextBlock();
+				tb.Text=String.Format("{0,3}",index);
+				tb.VerticalAlignment=VerticalAlignment.Center;
+				tb.Margin=new Thickness(0,0,2,0);
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,0);
+			}
+			{
+				TextBox tb = new TextBox();
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,1);
+				grid.ColumnDefinitions[1].Width=new GridLength(250,GridUnitType.Star);
+			}
+			{
+				TextBox tb = new TextBox();
+				tb.Text=String.Format("{0:HH:mm:ss}",dic.Key);
+				tb.HorizontalContentAlignment=HorizontalAlignment.Right;
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,2);
+				grid.ColumnDefinitions[2].Width=new GridLength(80,GridUnitType.Star);
+			}
+			{
+				TextBlock tb = new TextBlock();
+				tb.Text=String.Format("{0:HH:mm:ss}",dic.Value);
+				tb.HorizontalAlignment=HorizontalAlignment.Right;
+				tb.VerticalAlignment=VerticalAlignment.Center;
+				grid.Children.Add(tb);
+				Grid.SetColumn(tb,3);
+				grid.ColumnDefinitions[3].Width=new GridLength(60,GridUnitType.Star);
+			}
+			item.Content=grid;
+			return item;
+		}
+	}
+}
